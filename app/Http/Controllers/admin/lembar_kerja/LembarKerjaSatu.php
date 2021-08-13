0d8880a1ea1f93cddb3bc21880de2ada5e7d688d
Raditya Nugra <68360964+RadityaNugra@users.noreply.github.com>
2021-08-13 14:54:38 +0700

    detail guru

--- app/Http/Controllers/admin/lembar_kerja/LembarKerjaSatu.php
+++ app/Http/Controllers/admin/lembar_kerja/LembarKerjaSatu.php
@@ -30,7 +30,7 @@ class LembarKerjaSatu extends Controller
                 ->addColumn('action', function ($data) {
                     $button = '<a href="' . $data->id . '"   id="' . $data->id . '" class="edit btn btn-danger text-white btn-sm"><i class="fas fa-file-pdf"></i></a>';
                     $button .= '&nbsp';
-                    $button .= '<a href="/admin/target_pembelajaran/detail/' . $data->id . '"   id="' . $data->id . '" class="edit btn btn-primary btn-sm"><i class="fas fa-search"></i></a>';
+                    $button .= '<a href="/admin/Lembar-kerja-1/{Lembar_kerja_1}' . $data->id . '"   id="' . $data->id . '" class="edit btn btn-primary btn-sm"><i class="fas fa-search"></i></a>';
                     $button .= '&nbsp';
                     $button .= '<a  href="/admin/target_pembelajaran/edit/' . $data->id . '" id="edit" data-toggle="tooltip"  data-id="' . $data->id . '" data-original-title="Edit" class="edit btn btn-warning btn-sm edit-post"><i class="fas fa-pencil-alt"></i></a>';
                     $button .= '&nbsp';
--- resources/views/admin/guru/detail.blade.php
+++ resources/views/admin/guru/detail.blade.php
@@ -1,10 +1,57 @@
 @extends('layout.master')
-@section('title')
-@section('judul')
-@section('breadcrump')
 
+@push('css')
+
+@endpush
+@section('title', 'SIFOS | Data guru')
+@section('judul','Detail Guru')
+@section('breadcrump')
+    {{-- breadcrump here --}}
+    <div class="breadcrumb-item active"><a href="{{ route('admin.dashboard') }}">Dashboard</a></div>
+<div class="breadcrumb-item"><a href="{{ route('admin.guru.index') }}">Guru</a></div>
+<div class="breadcrumb-item">Detail Guru</div>
 @endsection
 @section('main')
+    {{-- content here --}}
+    <div class="col-12 col-md-12 ">
+        <div class="card card-primary">
+          <div class="card-header">
+            <h4>Informasi Guru</h4>
+          </div>
+          <div class="card-body">
+              <div class="row">
+            <div class="form-group col-6">
+                <label>NIK</label>
+                <input type="text" class="form-control" placeholder="32323" disabled>
+              </div>
+              <div class="form-group col-6">
+                <label>Nama Guru</label>
+                <input type="text" class="form-control" placeholder="329304897" disabled>
+              </div>
+              <div class="form-group col-6">
+                <label>Jabatan</label>
+                <input type="text" class="form-control" placeholder="329304897" disabled>
+              </div>
+            
+              <div class="form-group col-6">
+                <label>Jurusan</label>
+                <input type="text" class="form-control" placeholder="329304897" disabled>
+              </div>
+              <div class="form-group col-6">
+                <label>No Telp</label>
+                <input type="text" class="form-control" placeholder="329304897" disabled>
+              </div>
+              <div class="form-group col-6" style="margin-top: 30px">
+                <a href="{{route('admin.guru.index')}}" type="button" class="btn btn-danger ">   Kembali</a>
+              </div>
+              
+            </div>
+            {{-- <div class="btn btn-danger"  >
+                <a href="" type="button" class="btn btn-danger "><i class="fas fa-backspace"></i>   Kembali</a>
+            </div> --}}
+          </div>
+        </div>
+      </div>
 
 @endsection
 @push('js')
