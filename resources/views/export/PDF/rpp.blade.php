43224697f7e089624459d51e893f7fd90847043d
waladahlmzaqi <walada204@gmail.com>
2021-09-10 21:08:05 +0700

    up rpp_pdf

---
+++ resources/views/export/PDF/rpp.blade.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lembar RPP</title>
+    {{-- font google --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Ribeye&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bowlby+One+SC&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Martel:wght@300&display=swap" rel="stylesheet">
+
+</head>
+
+<style>
+
+    .logo {
+        width: 100px;
+        height: 100px;
+        margin-top: -30px;
+        margin-left: 20px;
+        float: left;
+
+    }
+
+    .font1 {
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+    }
+
+    .font2 {
+        font-family: 'Ribeye', cursive;
+    }
+
+    .font3 {
+        font-family: sans-serif
+    }
+
+    .font4 {
+        font-family: 'Bowlby One SC', cursive;
+    }
+
+    .font5 {
+        font-family: 'Noto Sans', sans-serif;
+    }
+
+    .font6 {
+        font-family: 'Martel', serif;
+    }
+
+</style>
+
+<body>
+    <div>
+        <div>
+            <img class="logo" src="https://pbs.twimg.com/profile_images/753761871300218880/OC5yL5QZ.jpg" alt="">
+        </div>
+        <div style="margin-left: 20px">
+            <p class="font5" style="font-weight:bold; margin-top: -10px; font-size: 20px;">RENCANA PELAKSANAAN PEMBELAJARAN (RPP)</p>
+            <p class="font5" style="font-weight:bold; margin-top: -20px; margin-left: 260px; font-size: 20px;">SMK TARUNA BHAKTI</p>
+            <p class="" style="margin-top: -20px; margin-left: 277px;">PEMROGRAMAN DASAR</p>
+            <p class="font5" style="font-weight:bold; margin-top: -15px; margin-left: 250px;">TAHUN PELAJARAN 2020/2021</p>
+        </div>
+    </div>
+
+
+    <div style="margin-left: -20px; margin-top:20px;">
+        <table class="font5" border="1" style="border-collapse:collapse;">
+            <thead>
+                <tr style="font-size: 14px;">
+                    <th colspan="" style="padding: 7px;">Kelas/Semester</th>
+                    <td colspan="2" style="padding: 7px;">X/I Gasal (GASAL)</td>
+                </tr>
+                <tr style="font-size: 14px;">
+                    <th colspan="" style="width: 150px; padding: 7px;">Alokasi Waktu</th>
+                    <td colspan="2" style="padding: 7px;">4 JP</td>
+                </tr>
+            </thead>
+            <tbody style="">
+                <tr style="font-size: 10px;">
+                    <td rowspan="4" style="padding-left: 7px; padding-right: 7px;">
+                        <p style="margin-top: -60px; font-weight: bold;">Tujuan Pembelajaran :</p><br>
+                        <p style="margin-top: -45px;">Setelah melaksanakan pembelajaran diharapkan peserta didik dapat
+                            lebih memahami dan menerapkan intruksi thread dalam pemrograman aplikasi
+                            berorientasi obyek.
+                        </p>
+                    </td>
+                    <td style="text-align: center; font-weight:bold;">KD Pengetahuan</td>
+                    <td style="text-align: center; font-weight:bold;">KD Keterampilan</td>
+                </tr>
+                <tr style="font-size: 10px;">
+                    <td style="padding-left: 20px;">3.1. Menerapkan dasar dasar thread</td>
+                    <td style="padding-left: 20px;">4.1 Membuat instruksi -instruksi thread</td>
+                </tr>
+                <tr style="font-size: 10px;">
+                    <td style="text-align: center; font-weight:bold;">IPK KD Pengetahuan</td>
+                    <td style="text-align: center; font-weight:bold;">IPK KD Keterampilan</td>
+                </tr>
+                <tr style="font-size: 10px;">
+                    <td style="padding: 10px;">
+                        1.	Menjelaskan konsep dasar instruksi thread dalam pemrograman aplikasi berorientasi obyek. <br>
+                        2.	Menjelaskan prosedur instruksi thread  dalam pemrograman aplikasi berorientasi obyek. <br>
+                        3.	Menjelaskan prosedur instruksi thread  dalam pemrograman aplikasi berorientasi obyek.
+                    </td>
+                    <td style="padding: 10px;">
+                        1.	Menerapkan instruksi thread dalam pemrograman aplikasi berorientasi obyek. <br>
+                        2.	Merancang program aplikasi berorientasi obyek dengan penerapan instruksi thread. <br>
+                        3.	Membuat kode program aplikasi berorientasi obyek yang menerapkan instruksi thread. <br>
+                        4.	Menguji program aplikasi berorientasi obyek yang menerapkan instruksi thread.
+                    </td>
+                </tr>
+                <tr style="font-size: 10px;">
+                    <td style="font-weight: bold; padding: 10px">Materi Pembelajaran</td>
+                    <td colspan="2" style="padding: 10px;">
+                        Pertemuan ke-1 : Konsep dasar thread dalam oop. <br>
+                        Pertemuan ke-2 : Prosedur penggunaan thread dalam oop <br>
+                        Pertemuan ke-3 : Membuat program oop menerapkan thread.
+                    </td>
+                </tr>
+                <tr style="font-size: 10px;">
+                    <td style="padding: 10px;">
+                        <p style="margin-top: -0px;">
+                            <b>Model:</b>
+                            Project Based Learning <br>
+                            <b>Metode:</b>
+                            Tanya jawab, diskusi, penugasan, role playing <br>
+                            <b>Pendekatan:</b>
+                            Saintifik (Mengamati, menanya,menalar, mencoba, mengkomunikasikan)
+                        </p>
+                    </td>
+                    <td style="padding: 10px;" colspan="2" rowspan="2">
+                        <b>Langkah Pembelajaran:</b> <br>
+                        <b>•	Kegiatan Pendahuluan Awal (15 menit)</b> <br>
+                        1.	Mengucapkan salam dan membaca basmallah bersama untuk mengawali pelajaran. <br>
+                        2.	Mengecek kehadiran siswa dan memberi motivasi belajar. <br>
+                        3.	Mengaitkan pengetahuan tentang bilangan pada perhitungan kehidupan nyata dengan materi sistem bilangan komputer <br>
+                        4.	Menjelaskan tujuan pembelajaran dan kompetensi dasar yang akan dicapai. <br>
+
+                        <b>•	Kegiatan Inti (45 menit)</b> <br>
+                        a.	Mengamati <br>
+                        -	Dasar Penggunaan ThreadMendesain Perencanaan Proyek.
+                        b.	Menanya <br>
+                        -	Mendiskusikan Manfaat ThreadMemonitor Peserta Didik dan Kemajuan Projek
+                        c.	Eksperimen/Eksplorasi
+                        –	Mengeksplorasi tentang penggunaan ThreadMengevaluasi
+                        d.	Asosiasi <br>
+                        -	 Membuat Kesimpulan tentang penggunaan Thread
+                        e.	Mengkomunikasikan <br>
+                        -	 Menyampaikan hasil pengamatan Penggunaan Thread dalam Pemrograman Berorientasi Objek
+                        <br>
+
+                        <b>•	Penutup (15 menit)</b> <br>
+                        a.	Siswa bersama guru menyimpulkan pembelajaran <br>
+                        b.	Siswa melakukan refleksi terhadap kegiatan yang sudah dilakukan <br>
+                        c.	Siswa dan guru melakukan umpan balik <br>
+                        d.	Siswa dan guru merencanakan tindak lanjut pembelajaran untuk pertemuan berikutnya
+
+                    </td>
+                </tr>
+                <tr style="font-size: 10px;">
+                    <td style="padding: 10px;">
+                        <b>Alat dan Bahan:</b>
+                        1.	Laptop / PC
+                        2.	Software Glifydiagram (sejenis)
+                        <br><br>
+                        <b>Media:</b>
+                        1.	Modul Pembelajaran
+                        2.	Video Pembelajaran
+                    </td>
+                </tr>
+                <tr style="font-size: 10px;">
+                    <td colspan="3" style="padding-left: 30px; padding-top: 10px; padding-bottom: 10px;" >
+                        <b>Asesmen:</b> <br>
+                        Penilaian Pembelajaran <br>
+                        1.	Sikap              : Observasi, Penilaian Diri, Penilaian Antar Teman*) <br>
+                        2.	Pengetahuan  : Tes tertulis, Lisan, Penugasan, Portofolio*) <br>
+                        3.	Ketrampilan    : Kinerja , Proyek, Portofolio*)
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+
+    <div style="margin-top: 30px;">
+        <div style="float: left; font-size: 12px; margin-left: 20px;">
+            <p>Mengetahui,</p>
+            <p style="margin-top: -10px; margin-bottom: 70px;">Kepala SMK Taruna Bhakti</p>
+            <p style="font-weight: bold;">Ramadin Tarigan, ST</p>
+        </div>
+        <div style="float: right; font-size: 12px;">
+            <p>Depok, Juli 2020</p>
+            <p style="margin-top: -10px; margin-bottom: 70px; margin-right: 80px;">Guru Mapel Sistem Komputer</p>
+            <p style="font-weight: bold;">Erraldo Daniel Siahaya</p>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
